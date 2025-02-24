f1bda5c0257f445a48475962165401bb6e21a948
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2025-02-24 04:30:29 +0300

    added swagger documentation

--- src/main/java/com/gpsolutions/hoteltask/HotelTaskApplication.java
+++ src/main/java/com/gpsolutions/hoteltask/HotelTaskApplication.java
@@ -3,7 +3,6 @@ package com.gpsolutions.hoteltask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class HotelTaskApplication {
 
